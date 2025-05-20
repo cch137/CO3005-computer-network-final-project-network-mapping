393b6368881ad7c1ec1698ae3403250653347282
cch137 <cheechorngherng@gmail.com>
2025-05-21 04:52:38 +0800

    .

--- network-topology/next.config.mjs
+++ network-topology/next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  webpack: (config) => {
+    config.resolve.alias["@"] = config.resolve.alias["@"] || process.cwd();
+    return config;
+  },
+};
 
-export default nextConfig
+export default nextConfig;
